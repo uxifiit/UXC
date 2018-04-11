59b39fa21b101a384b41c742959d1e39479c706c
Martin Konopka <martkono@gmail.com>
2018-04-11 12:15:12 +0200

    added nuget packaging for needed core libraries

--- build.bat
+++ build.bat
@@ -17,10 +17,12 @@ REM Build
 "%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" "UXC Solution.sln" /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
 REM Package
-REM mkdir Build
+mkdir Build
 
-REM for %%p in (
-REM <project>
-REM ) do (
-REM 	call "%nuget%" pack ".nuget\%%p.nuspec" -symbols -o Build -p Configuration=%config% 
-REM )
+for %%p in (
+    UXC.Core.Interfaces
+    UXC.Core.Data
+    UXC.Core.Data.Serialization.Json
+) do (
+ 	call "%nuget%" pack ".nuget\%%p.nuspec" -symbols -o Build -p Configuration=%config% 
+)
