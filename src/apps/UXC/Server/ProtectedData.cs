654d3d2dd7188ca1c53e6d1bc8a0bba7ec562817
Martin Konopka <martkono@gmail.com>
2018-04-25 17:48:05 +0200

    fixed bug with signalr not running in certain cases without admin account

---
+++ src/apps/UXC/Server/ProtectedData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Infrastructure;
+
+namespace UXC.Server
+{
+    class ProtectedData : IProtectedData
+    {
+        public string Protect(string data, string purpose)
+        {
+            return data;
+        }
+
+        public string Unprotect(string protectedValue, string purpose)
+        {
+            return protectedValue;
+        }
+    }
+}
