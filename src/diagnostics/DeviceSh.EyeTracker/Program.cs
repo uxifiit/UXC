2d48f26e1960f127278fb200d9e026f55c333e06
Martin Konopka <martkono@gmail.com>
2018-04-25 19:01:11 +0200

    added eye tracker console diagnostics app

---
+++ src/diagnostics/DeviceSh.EyeTracker/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using UXC.Core.Modules;
+using UXC.Devices.EyeTracker;
+using UXC.Devices.EyeTracker.Configuration;
+
+namespace DeviceSh.EyeTracker
+{
+    class EyeTrackerConfiguration : IEyeTrackerConfiguration
+    {
+        public string SelectedDriver
+        {
+            get
+            {
+                return null;
+            }
+            set
+            {
+            }
+        }
+    }
+
+    class NullModulesService : IModulesService
+    {
+        public void Register<T>(object target, Action<IEnumerable<T>> callback)
+        {
+        }
+
+        public void Unregister(object target)
+        {
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JsonSerializer serializer = new JsonSerializer();
+
+            var finder = new UXC.Devices.EyeTracker.TobiiPro.TobiiProFinder();
+            var config = new EyeTrackerConfiguration();
+            var modules = new NullModulesService();
+
+            var browser = new TrackerBrowser(new List<ITrackerFinder>() { finder }, config, modules);
+            var device = new EyeTrackerDevice(browser);
+
+            bool connected = device.ConnectToDevice();
+
+            device.Log += Device_Log;
+
+            if (connected)
+            {
+                device.DumpInfo();
+
+                device.DisconnectDevice();
+
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Failed to connect to tracker.");
+            }
+        }
+
+        private static void Device_Log(UXC.Core.Devices.IDevice device, UXC.Core.LogMessage info)
+        {
+            Console.WriteLine($"{info.Level} {info.Tag} {info.Message}");
+            if (info.Content != null)
+            {
+                var content = JsonConvert.SerializeObject(info.Content, Formatting.Indented);
+                Console.WriteLine(content);
+            }
+        }
+    }
+}
