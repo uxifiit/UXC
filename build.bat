0086eff25233035c08fc3897179a44f52ee3dfb9
Martin Konopka <martkono@outlook.com>
2018-06-04 19:40:42 +0200

    removed nuget build step for non-existing project

--- build.bat
+++ build.bat
@@ -23,7 +23,6 @@ for %%p in (
     UXC.Core.Interfaces
     UXC.Core.Data
     UXC.Core.Data.Serialization
-    UXC.Core.Data.Serialization.Json
 ) do (
  	call "%nuget%" pack ".nuget\%%p.nuspec" -symbols -o Build -p Configuration=%config% 
 )
